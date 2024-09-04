f29cb5c669f6aa69726bcdefc6667de55bdd12b0
ygouguasse <ygouguasse@gmail.com>
2024-09-04 10:26:25 -0400

    Lecture capteur

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("allo")
+import RPi.GPIO as GPIO
+import dht11
+import time
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+# GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 17)
+
+while True:
+
+    result = instance.read()
+
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+
+    time.sleep(7)
